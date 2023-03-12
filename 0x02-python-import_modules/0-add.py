4c1bb8fce455a0e1b422fae8cf069b05571cdb30
torohkiss <torohkiss@yahoo.com>
2023-03-11 17:27:01 -0800

    corr o

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__maim__":
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print(f"{} + {} = {add(a, b)}")
+    print(f"{a} + {b} = {add(a, b)}")
