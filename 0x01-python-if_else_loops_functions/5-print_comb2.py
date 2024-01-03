3027aec2cec4ef1a4d72d09bf13be2bf953c294d
torohkiss <torohkiss@yahoo.com>
2024-01-02 16:41:27 -0800

    task 5a

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0,100):
     if i != 99:
-        print(f"{i:02},", end=" ")
+        print("{0:02},".format(i), end=" ")
     else:
-        print(f"{i}")
+        print("{:d}".format(i))
