6f4b535c783339c9d12078bdd50a7013371adcfb
torohkiss <torohkiss@yahoo.com>
2024-01-04 06:05:30 -0800

    task 2f

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+
+    for i, v in enumerate(sys.argv):
+        if v != sys.argv[0]:
+            print("{}: {}".format(i, v))
