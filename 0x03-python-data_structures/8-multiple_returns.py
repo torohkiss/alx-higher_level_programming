dd8daee63a36eb7b5587d72e2a3f1b94369b61d3
torohkiss <torohkiss@yahoo.com>
2024-01-08 14:24:31 -0800

    task 8a

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return (0, None)
+        return (0, "None")
     l = len(sentence)
     c = sentence[0]
-
     return (l, c)
