c91df2b09a4e15c339f1a1cbc97baad91c6d5f20
torohkiss <torohkiss@yahoo.com>
2024-01-23 07:57:52 -0800

    task 0a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             total += 1
-    except IndexError:
+    except:
         pass
-    finally:
-        print()
+    print()
     return total
