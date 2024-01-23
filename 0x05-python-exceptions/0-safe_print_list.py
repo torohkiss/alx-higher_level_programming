7d226b19fbe284305498542ec0e9aa8f409e6d83
torohkiss <torohkiss@yahoo.com>
2024-01-23 08:00:38 -0800

    task 9b

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     total
     try:
-        for i in range(x):
-            print(my_list[i], end="")
+        for i in my_list[:x]:
+            print("{}".format(i), end="")
             total += 1
     except:
         pass
