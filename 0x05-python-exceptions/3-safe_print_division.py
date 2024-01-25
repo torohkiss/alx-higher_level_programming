087530957845a42d53535a65366ddc0d7a42b00c
torohkiss <torohkiss@yahoo.com>
2024-01-24 22:05:53 -0800

    task 3a

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except:
-        print("Inside result: {}".format(result))
+        return None
     finally:
         print("Inside result: {}".format(result))
     return result
