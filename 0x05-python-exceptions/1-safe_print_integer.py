dddac470acb5c5632ec4ea45e224009ff342f24e
torohkiss <torohkiss@yahoo.com>
2024-01-24 20:24:00 -0800

    task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        return False
