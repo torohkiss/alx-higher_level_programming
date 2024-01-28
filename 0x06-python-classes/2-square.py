b4760d59c7e13ccf5f100249c0bbcad9163e29e7
torohkiss <torohkiss@yahoo.com>
2024-01-27 23:30:33 -0800

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A square class with size"""
+
+
+class Square:
+    """Initializing size as private instance
+    attribute
+    """
+
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
