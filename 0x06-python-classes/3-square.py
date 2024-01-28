2775feae66803d0141413b5e0eb6c33d6c16b013
torohkiss <torohkiss@yahoo.com>
2024-01-27 23:44:54 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A square class with size"""
+
+
+class Square:
+    """Initializing size as private instance
+    attribute
+    """
+
+    def __init__(self, size=0):
+        """Checking size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
