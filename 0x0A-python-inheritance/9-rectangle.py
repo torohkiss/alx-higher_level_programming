ba56b65cea7c4e28f5739a3a38e154863969617b
torohkiss <torohkiss@yahoo.com>
2024-02-05 19:39:18 -0800

    task 9a

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,3 +19,8 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
