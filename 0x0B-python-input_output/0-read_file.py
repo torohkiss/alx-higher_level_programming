db7db1b818895941aec72d1fa200c54672b3f41c
torohkiss <torohkiss@yahoo.com>
2024-02-06 13:19:38 -0800

    task 0b

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,4 @@
 def read_file(filename=""):
     """read a file"""
     with open(filename, 'r', encoding='utf-8') as filee:
-        for line in filee:
-            print(line, end="")
+        print("{}".format(filee.read()), end="")
