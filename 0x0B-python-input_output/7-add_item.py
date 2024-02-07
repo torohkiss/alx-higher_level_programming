df9157976d91be31d6a08c0cb75e9d027a397824
torohkiss <torohkiss@yahoo.com>
2024-02-06 20:53:47 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""load add save"""
+
+
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    my_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    my_list = []
+
+for argv in sys.argv[1:]:
+    my_list.append(argv)
+
+save_to_json_file(my_list, 'add_item.json')
