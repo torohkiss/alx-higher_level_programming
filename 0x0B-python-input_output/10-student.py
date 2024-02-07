5b3779555cc9a1d63e47ba728caa4ab68ece4fae
torohkiss <torohkiss@yahoo.com>
2024-02-07 03:36:36 -0800

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A student class"""
+
+
+class Student:
+    """Student class initialized"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=""):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                    attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)
+                    }
