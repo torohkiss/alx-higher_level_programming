e85c5cfbb297142c9ada138f9ad94c52cc43bbfb
torohkiss <torohkiss@yahoo.com>
2024-02-13 04:13:38 -0800

    task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for max_integer"""
+
+import unittest
+
+class TestMaxIntgerFunc(unittest.TestCase):
+    """The test class for max_integer"""
+
+
+    def test_max(self):
+        self.assertEqual(max_integer, 4)
