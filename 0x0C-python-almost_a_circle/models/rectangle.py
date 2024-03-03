6093cfbc490d106d8d0271dad6e547bb7fb6caf0
torohkiss <torohkiss@yahoo.com>
2024-03-02 16:10:18 -0800

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """returns string representation"""
+        return f"({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
