569b2ed6ed83cfa4a71ea06cc1c1c5d4ca2d5381
torohkiss <torohkiss@yahoo.com>
2024-03-03 06:32:35 -0800

    task sqa

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     """A square class that inherits from rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """The __init__ method"""
         super().__init__(size, size, x, y, id)
 
     """def __str__(self):
