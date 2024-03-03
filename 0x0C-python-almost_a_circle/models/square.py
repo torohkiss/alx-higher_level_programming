1778a2b9f6a12e5ad62a7eff603d4a1fdaa10806
torohkiss <torohkiss@yahoo.com>
2024-03-03 06:38:41 -0800

    task 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A square class defined"""
+
+
 from models.rectangle import Rectangle
 
 
@@ -9,7 +11,3 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """The __init__ method"""
         super().__init__(size, size, x, y, id)
-
-    """def __str__(self):
-        return '"[Square]" + "(" + str(self.id) + ")" + str(
-        self.x) + "/" + str(self.y) + " " + "-" + " " + str(size)'"""
