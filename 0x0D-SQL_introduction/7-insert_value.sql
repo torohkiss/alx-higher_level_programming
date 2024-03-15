430ac420c4e8ef86a3b875a43280f0584c2098f8
torohkiss <torohkiss@yahoo.com>
2024-03-15 02:47:46 -0700

    task 7d

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Insert a row into a table
 -- in database hbtn_0c_0
-INSERT INTO first_table(id, name)
-VALUES (98, 'Best School');
+INSERT INTO first_table(id, name) 
+VALUES (89, 'Best School');
