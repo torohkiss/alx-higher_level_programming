100a557f14d34832a5f6990a8b21779846293f5a
torohkiss <torohkiss@yahoo.com>
2024-04-08 16:55:20 -0700

    task 4a

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-if (process.argv[2] && process.argv[3]) {
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
-} else if (process.argv[2]) {
-  console.log(`${process.argv[2]} is undefined`);
-} else {
-  console.log('undefined is undefined');
-}
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
