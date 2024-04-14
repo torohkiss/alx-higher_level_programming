e64a81436f5877e44c03625c9d0458d464842736
torohkiss <torohkiss@yahoo.com>
2024-04-13 17:35:18 -0700

    task 5s

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    supers(size, size);
+    super(size, size);
     this.size = size;
   }
 }
