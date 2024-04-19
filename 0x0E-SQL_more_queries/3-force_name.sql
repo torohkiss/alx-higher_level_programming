3ceaab60883b930fa6e189bb2c8f138aab0d9862
torohkiss <torohkiss@yahoo.com>
2024-04-19 06:11:05 -0700

    task 3a

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,5 @@
 -- creates the table force_name
-CREATE TABLE force_name IF NOT EXISTS (
-	id INT, VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	VARCHAR(256) NOT NULL
+);
