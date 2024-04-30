231bda597ce1d9fd7fa333f963763ca82ea196a1
torohkiss <torohkiss@yahoo.com>
2024-04-29 21:00:09 -0700

    tsk 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# displays the size of the body of the response
+# displays size of the body of response of a url
 curl -s -w %{size_download}"\n" "$1" -o /dev/null
