b79baba9dd0f22d14144c6d58ccf3c30e9b98eb0
torohkiss <torohkiss@yahoo.com>
2024-05-01 09:43:13 -0700

    task 1a

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,10 +5,8 @@
 import urllib.request
 import sys
 
-#url = 'https://alx-intranet.hbtn.io/status'
-
 with urllib.request.urlopen(sys.argv[1]) as response:
     h = response.headers
 
 var = h.get('X-Request-Id')
-sys.stdout.write(var)
+print(var)
