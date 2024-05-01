fe02a86dcc3838b442df150581ff98c58e0ee98c
torohkiss <torohkiss@yahoo.com>
2024-05-01 16:13:11 -0700

    task 4a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,8 @@
 
 import requests
 
-r = requests.get('https://alx-intranet.hbtn.io/status')
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
 print(" - type:", type(r.text))
 print(" - content:", r.text)
