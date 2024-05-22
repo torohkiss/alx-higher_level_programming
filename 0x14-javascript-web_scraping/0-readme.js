63c70c20bcc19715fe40dbad145c9b0d52cb37c9
torohkiss <torohkiss@yahoo.com>
2024-05-21 21:24:09 -0700

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const filePath = process.argv[1];
+
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log('File contents:', data);
+});
