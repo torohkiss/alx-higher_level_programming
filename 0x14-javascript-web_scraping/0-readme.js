e6f3842ad15b68ca2e1e6db045dccc0c36727faf
torohkiss <torohkiss@yahoo.com>
2024-05-21 21:27:25 -0700

    task 0b

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.error('Error reading file:', err);
+    console.error(err);
     return;
   }
-  console.log('File contents:', data);
+  process.stdout.write(data);
 });
