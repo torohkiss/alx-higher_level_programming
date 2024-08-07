89eb9786f886ea0455dc487b67393d6c6e46cb2a
torohkiss <torohkiss@yahoo.com>
2024-08-07 16:37:24 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+
+const request = require('request');
+request(url, function (error, response, body) {
+  console.log('code:', response && response.statusCode);
+});
