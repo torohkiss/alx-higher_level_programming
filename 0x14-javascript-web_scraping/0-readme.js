9cf5fc95409be1e5e9966918d5e2f17aaca41028
torohkiss <torohkiss@yahoo.com>
2024-08-07 15:10:14 -0700

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+const path = process.argv[2]
+
+const content = 'C is super fun!\n'
+
+fs.writeFile('cisfun', content, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+}
+})
+
+fs.readFile(path, 'utf-8', (err, data
+) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(content);
+})
