0031454387f405035aefe01993fd543cb5184643
torohkiss <torohkiss@yahoo.com>
2025-02-24 12:37:04 +0000

    task 0ag

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""function that prints all integers of a list"""
+
 
 def print_list_integer(my_list=[]):
+    # function that prints all integers of a list.
+
     for i in my_list:
         print("{}".format(i))
