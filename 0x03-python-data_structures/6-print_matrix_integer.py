b70ccc3ee3d7a30117046e2ceb761ac26944b133
torohkiss <torohkiss@gmail.com>
2025-02-24 18:33:51 +0100

    removed extra spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
-            print("{:d}".format(i[j]), end=" ")
+            print("{:d}".format(i[j]), end="")
             if j != len(i) - 1:
                 print(" ", end="")
         print()
