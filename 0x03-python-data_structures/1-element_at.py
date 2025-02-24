a855561d70f84181d3cb039faf28020132a79c38
torohkiss <torohkiss@yahoo.com>
2025-02-24 12:52:33 +0000

    taskk

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
-    # function that retrieves an element from a list like in C
-
-    if idx < 0:
-        return None
-    if idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
