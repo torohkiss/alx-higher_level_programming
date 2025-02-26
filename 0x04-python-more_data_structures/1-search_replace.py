9e8e0df8cb60ba07eb1136e027d370820532b421
torohkiss <torohkiss@yahoo.com>
2025-02-26 11:17:01 +0000

    new commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
 
-    for i in range(1, len(new_list)):
+    for i in range(0, len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
     return new_list
