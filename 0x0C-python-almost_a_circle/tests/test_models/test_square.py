04db028ae0364483114d3944796db95239a81089
torohkiss <torohkiss@yahoo.com>
2025-03-03 15:30:48 +0000

    added more square tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -6,6 +6,8 @@ import io
 import unittest
 
 class TestSquare(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test_square_str(self):
         s1 = Square(5)
@@ -15,3 +17,34 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(str(s1), "[Square] (1) 0/0 - 5")
         self.assertEqual(str(s2), "[Square] (2) 2/0 - 2")
         self.assertEqual(str(s3), "[Square] (3) 1/3 - 3")
+
+    def test_square_area(self):
+        s1 = Square(5)
+        s2 = Square(2, 2)
+        s3 = Square(3, 1, 3)
+
+        self.assertEqual(s1.area(), 25)
+        self.assertEqual(s2.area(), 4)
+        self.assertEqual(s3.area(), 9)
+
+    def test_square_display(self):
+        s1 = Square(5)
+        s2 = Square(2, 2)
+        s3 = Square(3, 1, 3)
+
+        captured_output = io.StringIO()
+
+        with redirect_stdout(captured_output):
+            s1.display()
+        output1 = captured_output.getvalue()
+        expected1 = "#####\n#####\n#####\n#####\n#####\n"
+        self.assertEqual(output1, expected1)
+
+        captured_output.truncate(0)
+        captured_output.seek(0)
+
+        with redirect_stdout(captured_output):
+            s2.display()
+        output2 = captured_output.getvalue()
+        expected2 = "  ##\n  ##\n"
+        self.assertEqual(output2, expected2)
