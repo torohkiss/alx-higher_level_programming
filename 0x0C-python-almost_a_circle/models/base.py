76e5d93d69f918b47ab43d7ce596aa8941424e86
torohkiss <torohkiss@yahoo.com>
2025-03-04 12:08:53 +0000

    added to_json_string to the Base class with tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """The Base model class module"""
 
 
+import json
+
+
 class Base:
     """The base model class"""
 
@@ -13,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return []
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,12 @@
+from models.rectangle import Rectangle
 from models.base import Base
+import json
 import unittest
 
 
 class TestModels(unittest.TestCase):
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test_base(self):
         b1 = Base()
@@ -15,3 +19,16 @@ class TestModels(unittest.TestCase):
         self.assertEqual(b3.id, 3)
         self.assertEqual(b4.id, 12)
         self.assertEqual(b5.id, 4)
+
+    def test_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        expected_dict = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+
+        self.assertEqual(dictionary, expected_dict)
+        self.assertEqual(type(dictionary), dict)
+        self.assertEqual(json.loads(json_dictionary), [dictionary])
+        self.assertIsInstance(json_dictionary, str)
+
+
